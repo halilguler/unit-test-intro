04e033a60aa68f016ab4fe0c6126c34d3f6a3a13
Halil GÜLER <halilguler1994@gmail.com>
2022-07-21 14:08:55 +0300

    lint staged && prettier

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
-export function Hello(){
-    return <div>Hello World!</div>
+export function Hello() {
+  return <div>Hello World!</div>;
 }
